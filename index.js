721ad87495f236e7a0c1213dab1341d7726801d1
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-22 14:56:41 +0300

    Add fs module usage example

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
 'use strict';
-// node ./path/to/file.js - запуск файла с помощью node js
 
 // CommonJS imports
-const result = require('./MyMath');
-const { Component } = require('./Component');
+// const result = require('./MyMath');
+// const { Component } = require('./Component');
+// require('./src');
+// const { MyMath, PI } = result;
 
-require('./src');
-const { MyMath, PI } = result;
+const fs = require('fs/promises');
+// const fs2 = require('fs').promises;
 
-console.log('index.js');
+// console.log(fs);
 
-const value = 'some value';
-// const value = 'some value';
+// fs.readFile('./require_working_algo.txt').then((fileContent) => {
+//   const decodedText = Buffer.from(fileContent, 'utf-8').toString();
+//   console.log(decodedText);
+// });
 
-// console.log(MyMath.sum(5,10,20,30));
-// console.log(MyMath.divide(500,2));
+// fs.readFile('./require_working_algo.txt', { encoding: 'utf-8' }).then(
+//   (fileContent) => {
+//     console.log(fileContent);
+//   }
+// );
 
-/*
-  Этапы работы require
-    1. resolve - ищется файл
-      1.1 нода подключает модули ядра (Globals)
-      1.2 ищет файл по предоставленому пути
-        1.2.1 ищет файл с расширением .js
-        1.2.2 ищет файл с расширением .json
-      1.3 ищется папка по предоставленному пути
-        1.3.1 ищет в папке файл package.json и в нем свойство "main"
-        1.3.2 иначе ищет файл index.js | index.json
-      1.4 ищется папка node_modules
-      1.5 кидает ошибку
-    2. loading - файл читается
-    3. wrapping - нода оборачивает файл в функцию, в неё также передают инжектированные переменные
-    4. evaluation - запускает весь код в файле (функцию из пункта 3)
-    5. caching - результат работы из пункта 4 сохраняется с данными которые отдаются на экспорт. Код каждого файла запускается не более одного раза.
-*/
+fs.readFile('./require_working_algo.txt', 'utf-8').then((fileContent) => {
+  console.log(fileContent);
+});
+
